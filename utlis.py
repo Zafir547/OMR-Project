5d070bb61a7d52648113acc5f5b1c7d772d92710
Zafir Abdullah <63918801+Zafir547@users.noreply.github.com>
2024-04-26 11:41:50 +0500

    Update OMR Projects

--- utlis.py
+++ utlis.py
@@ -77,7 +77,30 @@ def reorder(myPoints):
 
 def splitBoxes(img):
     rows = np.vsplit(img,5)
-    cv2.imshow("Split",rows[0])
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r,5)
+        for box in cols:
+            boxes.append(box)
+            cv2.imshow("Split",box)
+    return boxes
 
+def showAnswers(img,myIndex,grading,ans,questions,choices):
+    secW = int(img.shape[1]/questions)        
+    secH = int(img.shape[0]/choices)
 
+    for x in range(0,questions):
+        myAns = myIndex[x]
+        cX = (myAns*secW)+secW//2
+        cY = (x*secH) + secH//2
 
+        if grading[x] == 1:
+            myColor = (0,255,0)
+        else:
+            myColor = (0,0,255)
+            correctAns = ans[x]
+            cv2.circle(img, ((correctAns*secW)+secW//2,(x*secH)+secH//2), 20, (0,255,0), cv2.FILLED)
+        
+        cv2.circle(img,(cX,cY),50,myColor,cv2.FILLED)
+
+    return img    
